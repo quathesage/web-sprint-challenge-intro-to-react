a50af9883e47bbb290c5056a8fb420606873691b
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2021-11-05 17:48:04 -0500

    added some styled components

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,6 +2,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  h3 {
+      font-size: 30px;
+      color: #FFE81F;
+      background-color: black;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      width: 25%;
+      margin: 20px auto;
+      padding: 5px;
+      border-radius: 10px;
+  }
+
+  p {
+      font-weight: bold;
+      font-family: sans-serif;
+      background-color: black;
+      color: #FFE81F;
+      width: 25%;
+      margin: 20px auto;
+  }
+`
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: flex-wrap;
+`
+
+
+
 
 
 
@@ -10,7 +40,7 @@ const Characters = (props) => {
     const { characterList } = props;
 
    return (
-       <div>
+       <Container>
              {characterList.map((character) => {
         return (
           <>
@@ -34,7 +64,7 @@ const Characters = (props) => {
           </>
         )
       })}
-       </div>
+       </Container>
    )
 }
 
